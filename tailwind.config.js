87d68739b09b376a15f4cf5c61b0f4115e002c7d
Shivam Kumar Chaubey <shivamc36@gmail.com>
2025-10-06 06:50:34 +0530

    bug: fixed the issue which is causing the metro bundler to stuck over the 99.9%

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
       fontFamily: {
+        "quicksand": ["QuickSand-Regular"],  // ADD THIS LINE
         "quicksand-bold": ["QuickSand-Bold"],
         "quicksand-medium": ["QuickSand-Medium"],
         "quicksand-regular": ["QuickSand-Regular"],
         "quicksand-semibold": ["QuickSand-SemiBold"],
         "quicksand-light": ["QuickSand-Light"],
+      },
+      colors: {
+        primary: "#FF6347", // Add your actual primary color
+        "dark-100": "#1F1F1F", // Add your actual dark color
       },
     },
   },
